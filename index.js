27a8013e1fca55685d276321521b861df5afa7a5
Mendel Minkowicz <mminkowicz@gmail.com>
2025-07-21 15:34:22 -0400

    Completed the JavaScript Expressions Lab

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-//! Start by creating the variables for the data recorded
-//* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
+// Temperature Data
+const celsiusTemps = [25, 18, 15, 28, 20, 23, 30, 22, 26, 24, 21, 27, 19, 17, 29];
+const fahrenheitTemps = [32, 70, 80, 72, 68, 75, 82, 65, 77, 78, 73, 79, 71, 74, 76];
 
+    const celsiusTempsToFahrenheit = celsiusTemps.map(temp => ((temp * 9 / 5) + 32));
+    const fahrenheitTempsToCelsius = fahrenheitTemps.map(temp => ((temp - 32) * 5 / 9));
 
-//! Start the calculation of the total temperatures
-//* Then apply the conversion to calculate the total in the other unit of measurement
-//* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
+    const tot_temperature_in_fahrenheit = 32 + 70 + 80 + 72 + 68 + 75 + 82 + 65 + 77 + 78 + 73 + 79 + 71 + 74 + 76;
+    const tot_temperature_in_celsius = 25 + 18 + 15 + 28 + 20 + 23 + 30 + 22 + 26 + 24 + 21 + 27 + 19 + 17 + 29;
 
-//! Start the calculation of the average temperatures
-//* Call the variables: avg_temperature_in_fahrenheit and avg_temperature_in_celsius
+    console.log("Total Temp C", tot_temperature_in_celsius)
+    console.log("Total Temp F",tot_temperature_in_fahrenheit)
 
-//! Console.log the results for your own inspection if you'd like
+    const avg_temperature_in_celsius = tot_temperature_in_celsius / celsiusTemps.length;
+    const avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / fahrenheitTemps.length;
 
-//! After creating the four variables mentioned above, uncomment the following lines
-//* This way you can export them to the test file, this is essential for the tests to work
-
-module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
-};
+    console.log("Celcius Average is", avg_temperature_in_celsius);
+    console.log("Fahrenheit Average is", avg_temperature_in_fahrenheit);
